a37544d6fd1606e1a6184a4bc1c7956fb40c5c50
LongNgoTapCode <124150776+LongNgoTapCode@users.noreply.github.com>
2025-02-21 00:43:15 +0700

    Update Register.cshtml.cs

--- ecomerce/Pages/Account/Register.cshtml.cs
+++ ecomerce/Pages/Account/Register.cshtml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using ecomerce.Models;
 using ecomerce.Service;
 using ecomerce.IService;
@@ -14,12 +11,14 @@ namespace ecomerce.Pages.Account
     public class RegisterModel : PageModel
     {
         private readonly IMailKitService _mailKitService;
-        private readonly AccountService accountService = new AccountService();
+        private readonly IAccountService _accountService;
 
-        public RegisterModel(IMailKitService mailKitService)
+        public RegisterModel(IMailKitService mailKitService, IAccountService accountService)
         {
             _mailKitService = mailKitService;
+            _accountService = accountService;
         }
+
         public IActionResult OnGet()
         {
             return Page();
@@ -28,16 +27,17 @@ namespace ecomerce.Pages.Account
         [BindProperty]
         public ecomerce.Models.Account Account { get; set; } = default!;
 
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-            if (accountService.checkAccountDuplicate(Account)) return Page();
-            accountService.registerAccountAsync(Account);
+            if (_accountService.CheckAccountDuplicate(Account)) return Page();
+            await _accountService.RegisterAccountAsync(Account);
+
             string email = Account.UserName;
             string name = Account.FullName;
-            string message = "Cam on " + name + " da dang ky dich vu cua chung toi";
+            string message = $"Cảm ơn {name} đã đăng ký dịch vụ của chúng tôi";
+            
             await _mailKitService.SendEmailAsync(email, "Confirm Register", message);
+
             return RedirectToPage("/Account/Login");
         }
     }
